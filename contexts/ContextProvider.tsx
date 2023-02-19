7111ab5e8834ee5a2f3d899f1bf3d8a4e036f932
dede ariansya <dedeariansya1@gmail.com>
2023-02-20 03:07:39 +0800

    ⏪ rollback: Remove ImageContext, ImageProvider, useImage, and ImageList components

--- contexts/ContextProvider.tsx
+++ contexts/ContextProvider.tsx
@@ -1,10 +1,5 @@
-import { ImageProvider } from './ImageContext'
 import { MountProvider } from './MountContext'
 
 export default function ContextProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <MountProvider>
-      <ImageProvider>{children}</ImageProvider>
-    </MountProvider>
-  )
+  return <MountProvider>{children}</MountProvider>
 }
