ecbbbccd0f663c0df3a80668fdf729b2c4bab6b6
dede ariansya <dedeariansya1@gmail.com>
2023-02-20 00:53:31 +0800

    🔵 other: Add a StarIcon component

--- components/icons/Feather.tsx
+++ components/icons/Feather.tsx
@@ -91,3 +91,10 @@ export const LockIcon = memo((props: SVGProps<SVGSVGElement>) => (
     <path d="M7 11V7a5 5 0 0110 0v4"></path>
   </svg>
 ))
+
+export const StarIcon = memo((props: SVGProps<SVGSVGElement>) => (
+  <svg {...initial} {...props}>
+    <title>Star Icon</title>
+    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+  </svg>
+))
