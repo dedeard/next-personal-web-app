b4c7fd2536840fb79e5be9d053ce3e0fd8a07da0
dede ariansya <dedeariansya1@gmail.com>
2023-02-20 02:37:39 +0800

    ✨feature: Add ClipboardIcon component

--- components/icons/Feather.tsx
+++ components/icons/Feather.tsx
@@ -88,3 +88,10 @@ export const StarIcon = memo((props: SVGProps<SVGSVGElement>) => (
     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
   </svg>
 ))
+
+export const ClipboardIcon = memo((props: SVGProps<SVGSVGElement>) => (
+  <svg {...initial} {...props}>
+    <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+    <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+  </svg>
+))
