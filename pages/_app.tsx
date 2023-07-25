6141625c61307eef49bd58e6e8878e4151207c43
Dede ariansya <dedeariansya1@gmail.com>
2023-07-26 04:58:14 +0800

    Make dark mode is default theme.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps, router }: AppProps<PageProps
     <>
       <GoogleAnalytics trackPageViews />
       <Analytics />
-      <ThemeProvider attribute="class">
+      <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark" storageKey="mode">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon-white.ico" media="(prefers-color-scheme: dark)" />
