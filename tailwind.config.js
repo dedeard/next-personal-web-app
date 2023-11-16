517a116cee5833f9229260754667917ce9549499
Dede Ariansya <me@dedeard.my.id>
2023-11-16 12:31:02 +0800

    🖥️ wip: setup app router and linter.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './contexts/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
