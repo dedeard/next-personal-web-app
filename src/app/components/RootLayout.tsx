893dd0f95e6851cb8224363873cf82015a766a33
Dede Ariansya <me@dedeard.my.id>
2023-12-02 15:43:28 +0800

    🖥️ wip: remove preloader and rename layout component.

--- src/app/components/RootLayout.tsx
+++ src/app/components/RootLayout.tsx
@@ -2,17 +2,15 @@
 import { ThemeProvider } from 'next-themes'
 import CursorFollower from './CursorFollower'
 import NoiseFilter from './NoiseFilter'
-import Preloader from './Preloader'
 import Header from './Header'
 import Footer from './Footer'
 import RootBackground from './RootBackground'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <NoiseFilter />
       <CursorFollower />
-      <Preloader />
       <Header />
       <main className="relative flex-1 bg-white/60 p-3 dark:bg-black/75 md:px-5 lg:px-10">{children}</main>
       <Footer />
@@ -20,3 +18,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </ThemeProvider>
   )
 }
+
+export default RootLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { Lato } from 'next/font/google'
-import Layout from './components/Layout'
+import RootLayout from './components/RootLayout'
 
 const lato = Lato({
   weight: ['400', '700'],
@@ -9,11 +9,11 @@ const lato = Lato({
   display: 'swap',
 })
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={'dark h-full w-full ' + lato.className} style={{ colorScheme: 'dark' }}>
       <body className="flex min-h-full w-full overflow-y-scroll bg-white pb-16 text-black antialiased selection:bg-yellow-600 dark:bg-black  dark:text-white md:pb-0 md:pl-16">
-        <Layout>{children}</Layout>
+        <RootLayout>{children}</RootLayout>
         <Analytics />
       </body>
     </html>
