abf0abf18e9e9caa756c2768c987c74305280bae
Dede Ariansya <me@dedeard.my.id>
2023-12-08 00:10:13 +0800

    🖥️ wip: remove useMounte and fix progress.

--- src/app/projects/components/LangBar.tsx
+++ src/app/projects/components/LangBar.tsx
@@ -1,18 +1,13 @@
 'use client'
 import { memo } from 'react'
-import { useMount } from '@/hooks/mount'
 import { useSpring, animated } from '@react-spring/web'
 
 const LangBar: React.FC<{ size: number; color?: string; className?: string }> = ({ size, color, className }) => {
-  const mount = useMount()
-
   const props = useSpring({
-    from: { width: '0px' },
+    from: { width: '0%' },
     to: { width: `${size}%` },
-    config: { delay: 1000, duration: 1000 },
+    config: { duration: 1000 },
   })
-
-  if (!mount) return null
 
   return <animated.div style={{ ...props, backgroundColor: color }} className={className} />
 }
