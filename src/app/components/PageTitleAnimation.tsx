1f6d7c48bf0b1231ce4145cbb43c3e51e7816dc2
Dede Ariansya <me@dedeard.my.id>
2023-12-08 00:09:35 +0800

    🖥️ wip: remove animation delay.

--- src/app/components/PageTitleAnimation.tsx
+++ src/app/components/PageTitleAnimation.tsx
@@ -26,7 +26,7 @@ const PageTitleAnimation: React.FC = () => {
       {mounted && (
         <span className="fixed top-0 -z-40 block select-none px-3 text-[calc(2rem+6.9vw)] md:left-16 md:px-5 lg:px-10 ">
           <span className="block text-[1.5em] font-bold md:text-[1.6em]">
-            <TextTransition springConfig={config.wobbly} inline delay={400}>
+            <TextTransition springConfig={config.wobbly} inline>
               <span data-text={title} className="after:content-[attr(data-text)]" />
             </TextTransition>
             <span data-text="." className="after:content-[attr(data-text)]" />
