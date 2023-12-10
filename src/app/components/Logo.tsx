d838052f2706aee50a6c8e8586989abe4de15223
Dede Ariansya <me@dedeard.my.id>
2023-12-10 16:49:12 +0800

    🖥️ wip: update prop types.

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,4 +1,4 @@
-const Logo = (props: React.SVGProps<SVGSVGElement>) => (
+const Logo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 137.41 137.41" height={16} width={16} {...props}>
     <path d="M68.71 0a68.71 68.71 0 1068.7 68.71A68.71 68.71 0 0068.71 0zm26.21 82.92a12 12 0 01-12 12H54.49a12 12 0 01-12-12V54.49a12 12 0 0112-12h28.43a12 12 0 0112 12z"></path>
   </svg>
