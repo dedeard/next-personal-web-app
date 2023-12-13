154ea09f62a5d60d2bb0a4d643b9f9a817068b23
Dede Ariansya <me@dedeard.my.id>
2023-12-13 15:58:10 +0800

    ✨feature: complete read blog page.

--- src/app/(root)/blog/[slug]/page.tsx
+++ src/app/(root)/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { getBlogPosts } from '@/utils/get-blog-posts'
-import timeFromNow from '@/utils/time-from-now'
 import { HOST } from '@/constans/common'
+import formatDate from '@/utils/format-date'
 import MDX from './components/MDX'
 
 export const revalidate = 3600
@@ -16,7 +16,6 @@ export function generateMetadata({ params }: { params: { slug: string } }): Meta
     authors: {
       name: 'Dede Ariansya',
     },
-    robots: 'index, follow',
     openGraph: {
       title: post.metadata.title,
       description: post.metadata.summary,
@@ -29,18 +28,6 @@ export function generateMetadata({ params }: { params: { slug: string } }): Meta
   }
 }
 
-function formatDate(date: string) {
-  let datetime = new Date(date)
-
-  let fullDate = datetime.toLocaleString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  })
-
-  return `${fullDate} (${timeFromNow(date)})`
-}
-
 export default async function BlogDetailPage({ params }: { params: { slug: string } }) {
   let post = getBlogPosts().find((post) => post.slug === params.slug)
   if (!post) {
@@ -48,7 +35,7 @@ export default async function BlogDetailPage({ params }: { params: { slug: strin
   }
 
   return (
-    <main className="relative z-10 flex-1 p-3 md:px-5 lg:px-10">
+    <main className="relative z-10 flex-1 bg-neutral-50 p-3 pt-10 dark:bg-neutral-950/30 md:px-5 md:pb-10 lg:px-10">
       <script
         type="application/ld+json"
         suppressHydrationWarning
@@ -69,11 +56,11 @@ export default async function BlogDetailPage({ params }: { params: { slug: strin
           }),
         }}
       />
-      <h1 className="title max-w-[650px] text-2xl font-medium tracking-tighter">{post.metadata.title}</h1>
+      <h1 className="max-w-[650px] text-3xl font-bold tracking-tighter md:text-4xl xl:text-5xl">{post.metadata.title}</h1>
       <div className="mb-8 mt-2 flex max-w-[650px] items-center justify-between text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">{formatDate(post.metadata.publishedAt)}</p>
+        <p className="text-sm opacity-60">{formatDate(post.metadata.publishedAt)}</p>
       </div>
-      <article className="prose prose-neutral dark:prose-invert">
+      <article className="prose max-w-[650px] dark:prose-invert">
         <MDX source={post.content} />
       </article>
     </main>
