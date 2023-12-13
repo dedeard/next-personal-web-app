f0b7a4dd84910a5abbff37981299cf3cef34ea68
Dede Ariansya <me@dedeard.my.id>
2023-12-13 15:47:32 +0800

    ✨feature: complete blog lists page.

--- src/app/(root)/(app)/blog/page.tsx
+++ src/app/(root)/(app)/blog/page.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
-import PageTitle from '../components/PageTitle'
-import { getBlogPosts } from '@/utils/get-blog-posts'
 import Link from 'next/link'
+import { getBlogPosts } from '@/utils/get-blog-posts'
+import formatDate from '@/utils/format-date'
+import PageTitle from '../components/PageTitle'
 
 export const metadata: Metadata = {
   title: 'Blog - Dede Ariansya',
@@ -25,18 +26,16 @@ export default async function BlogPage() {
   return (
     <>
       <PageTitle title="Blog" />
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
-        <ul>
-          {blogs.map((post) => (
-            <li key={post.slug}>
-              <Link href={'/blog/' + post.slug} className="my-3 block border">
-                <h3>{post.metadata.title}</h3>
-                <p>{post.metadata.summary}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className="grid grid-cols-1 gap-3 md:mb-5">
+        {blogs.map((post) => (
+          <li key={post.slug}>
+            <Link href={'/blog/' + post.slug} className="block bg-white/50 px-3 py-5 backdrop-blur dark:bg-black/80 md:px-5">
+              <h3 className="text-lg md:text-xl">{post.metadata.title}</h3>
+              <p className="text-sm opacity-60 dark:text-neutral-400">{formatDate(post.metadata.publishedAt)}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
