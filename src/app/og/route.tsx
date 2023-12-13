3ea8ee74f73de9937e6dd869f8e834aee2a9c756
Dede Ariansya <me@dedeard.my.id>
2023-12-13 12:09:49 +0800

    ✨feature: create og image generator.

---
+++ src/app/og/route.tsx
@@ -0,0 +1,57 @@
+import { ImageResponse } from 'next/og'
+import { NextRequest } from 'next/server'
+import { HOST } from '@/constans/common'
+
+export const runtime = 'edge'
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl
+  const postTitle = searchParams.get('title')
+  const fontData = await fetch(new URL('fonts/Lato-Bold.ttf', HOST)).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          backgroundImage: `url(${HOST}/media/og-bg.jpg)`,
+        }}
+      >
+        <div
+          style={{
+            paddingLeft: 190,
+            paddingRight: 190,
+            margin: 'auto',
+            display: 'flex',
+            fontSize: 130,
+            fontFamily: 'Lato',
+            letterSpacing: '-0.05em',
+            fontStyle: 'normal',
+            color: 'white',
+            lineHeight: '120px',
+            whiteSpace: 'pre-wrap',
+            textAlign: 'center',
+          }}
+        >
+          {postTitle}
+        </div>
+      </div>
+    ),
+    {
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: 'Lato',
+          data: fontData,
+          style: 'normal',
+        },
+      ],
+    },
+  )
+}
