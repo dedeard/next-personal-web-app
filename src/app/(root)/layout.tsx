43e78af611a7e0c15ad7dbf8dab9c1a3266aa043
Dede Ariansya <me@dedeard.my.id>
2023-12-14 00:23:44 +0800

    🐞 fix: hiding footer when using mobile.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex flex-1 pb-16 md:pb-0 md:pl-16">
         <Navigation />
         {children}
-        <footer className="pointer-events-none fixed bottom-0 right-0 z-[100] select-none p-3 text-xs md:visible md:p-5 lg:px-10">
+        <footer className="pointer-events-none invisible fixed bottom-0 right-0 z-[100] select-none p-3 text-xs md:visible md:p-5 lg:px-10">
           <p>© {new Date().getFullYear()} DedeArd. All rights reserved.</p>
         </footer>
       </div>
