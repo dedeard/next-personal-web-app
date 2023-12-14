62f6bc690dde4094f856ce312da923a48b130e47
Dede Ariansya <me@dedeard.my.id>
2023-12-14 14:38:25 +0800

    🐞 fix: page preloader rerender every change page.

--- src/app/components/Preloader.tsx
+++ src/app/components/Preloader.tsx
@@ -1,17 +1,24 @@
 'use client'
 
-import { memo } from 'react'
-import { animated, useSpring } from '@react-spring/web'
+import { memo, useEffect, useState } from 'react'
 import { useMount } from '@/contexts/MountContext'
 
 const Preloader: React.FC = () => {
   const mounted = useMount()
-  const props = useSpring({ opacity: mounted ? 0 : 1, delay: 500 })
+  const [hide, setHide] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHide(true)
+    }, 2200)
+  }, [])
+
+  if (hide) return null
 
   return (
-    <animated.div
-      style={props}
-      className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-[130] h-full w-full select-none bg-white dark:bg-black"
+    <i
+      style={{ opacity: mounted ? 0 : 1 }}
+      className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-[130] block h-full w-full select-none bg-white transition-opacity delay-[1s] duration-[1s] dark:bg-black"
     />
   )
 }
