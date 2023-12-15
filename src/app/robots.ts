42f19e870ba91b14275a65eaee94b221eea6d36a
Dede Ariansya <me@dedeard.my.id>
2023-12-15 22:57:00 +0800

    ⏪ rollback: remove robots and sitemap.

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from 'next'
-import { HOST } from '@/constans/common'
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: `${HOST}/sitemap.xml`,
-  }
-}
